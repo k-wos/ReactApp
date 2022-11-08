be0362298f8dcff782ec1860368e095e3c2556fc
k-wos <kacper.wos33@gmail.com>
2022-11-08 16:04:39 +0100

    nav

--- my-app/src/components/NavBar/NavBar.tsx
+++ my-app/src/components/NavBar/NavBar.tsx
@@ -4,21 +4,38 @@ import styled from 'styled-components';
 
 const Menu = styled.nav`
     margin: 0 auto;
-    background-color: #343e43;
-    position: relative;
+    background-color: #13d6d6;
+    //box-shadow: 0px 2px 10px rgba(0,0,0,0.15);
+    position: sticky;
     text-align: center;
     width:100%;
+    top:0;
+    height: 80px;
+    display:flex;
+    justify-content:center;
+    align-items:center;
+    z-index:999;
     
     
 `;
 const NavLink = styled(Link)`
-    color: #808080;
-    align-items: center;
-    text-decoration: none;
-    padding: 0 1rem;
-    height: 100%;
+    @import url('https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;800;900&family=Rubik:wght@800&display=swap');
+    text-decoration:none;
+    font-weight:bold;
+    font-size:1rem;
+    color:#fff;
+    display:flex;
+    justify-content:space-between;
+    align-items:center;
+    height:100%;
+    padding: 1rem 2rem;
+    transition: all .2s ease;
+    font-family: 'Nunito', sans-serif;
+
+    
     &:hover{
-        color:red;
+        color: #e30657;
+        transform: translateY(-2rem);
     }
 
 `;
